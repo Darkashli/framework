54b28c24d6fdd6525deb0194b3c8e430fe12ccc4
Mhd Alaa Darkashli <99034433@mydavinci.nl>
2017-05-11 11:19:32 +0200

    The first step

---
+++ view/student/view.php
@@ -0,0 +1,6 @@
+<div style="background-color: skyblue">
+<h1>Model-view-controller-model</h1>
+
+<p>Model-view-controller (of MVC) is een ontwerppatroon ("design pattern") dat het ontwerp van complexe toepassingen opdeelt in drie eenheden met verschillende verantwoordelijkheden: datamodel (model), datapresentatie (view) en applicatielogica (controller). Het scheiden van deze verantwoordelijkheden bevordert de leesbaarheid en herbruikbaarheid van code. Het maakt ook dat bijvoorbeeld veranderingen in de gebruikersinterface niet direct invloed hebben op het datamodel en vice versa. MVC werd voor het eerst gebruikt in de eerste implementaties van Smalltalk.<p>
+
+<div>
---
+++ view/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Studenten app</title>	
+	<link rel="stylesheet" href="<?= URL ?>">
+</head>
+<body>
+	<nav>
+	<ul>
+		<li><a href="<?= URL ?>home/index">Home</a></li>
+		<li><a href="<?= URL ?>student/index">Students</a></li>
+		<li><a href="<?= URL ?>student/view">MVC</a></li>
+		<li><a href="<?= URL ?>student/view.2">Example</a></li>
+
+
+
+		
+
+
+	</ul>
+	</nav>
