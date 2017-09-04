5a3f6eb8a8699451d00a1e49014ff471fdac5be2
Alaa Darkashli <99034433@mydavinci.nl>
2017-09-04 12:21:50 +0200

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,10 +1,10 @@
 <?php
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'framework');
 define('DB_USER', 'root');
-define('DB_PASS', '207218');
+define('DB_PASS', '');
 define('DB_CHARSET', 'utf8');
 
 define('URL_PUBLIC_FOLDER', 'public');
